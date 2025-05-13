3c5e9114080958250a17a78940a9b16fdd8c5d8b
CHOUDHARY Devender <devender.choudhary@hexagon.com>
2025-05-13 21:05:01 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            TestViewModel testViewModel = new TestViewModel { Name = "Photons",
+            DateOfBirth = new DateTime(1996, 12, 29)
+            };
+
+            return View(testViewModel);
+        }
+    }
+}
